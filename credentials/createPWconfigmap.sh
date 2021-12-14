7cdd379c1b45662ad9db0c9afdb8d735fe23eefe
cccaternberg <acaternberg@cloudbees.com>
2021-12-14 15:41:04 +0100

    add

--- credentials/createPWconfigmap.sh
+++ credentials/createPWconfigmap.sh
@@ -3,4 +3,7 @@ kubectl delete configmap simple-pw-config
 kubectl create configmap simple-pw-config \
        --from-env-file=pw.properties
 
+kubectl delete -f mysecrets.yaml
+kubectl create -f mysecrets.yaml
+
 

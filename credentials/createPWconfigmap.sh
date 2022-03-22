762090f93d471568e18f071085137aeb22c15c0c
cccaternberg <acaternberg@cloudbees.com>
2022-03-22 15:51:03 +0100

    cleanup

--- credentials/createPWconfigmap.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-kubectl delete configmap simple-pw-config
-kubectl create configmap simple-pw-config \
-       --from-env-file=pw.properties
-
-kubectl delete -f mysecrets.yaml
-kubectl create -f mysecrets.yaml
-
-

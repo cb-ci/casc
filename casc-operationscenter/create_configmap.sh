9964cacc3f937f72ce8e7431aff2ab2f00cc7833
cccaternberg <acaternberg@cloudbees.com>
2022-10-07 10:41:13 +0200

    add

--- casc-operationscenter/create_configmap.sh
+++ casc-operationscenter/create_configmap.sh
@@ -5,4 +5,4 @@ NS=cloudbees-core
 kubectl -n $NS  delete cm oc-casc-bundle
 #kubectl -n $NS  create cm  oc-casc-bundle --from-file=bundle --dry-run=client -o yaml
 kubectl -n $NS  create cm  oc-casc-bundle --from-file=bundle
-kubectl -n $NS  delete pod cjoc-0
+#kubectl -n $NS  delete pod cjoc-0

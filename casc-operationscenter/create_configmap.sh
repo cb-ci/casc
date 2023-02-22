09d17aefd2b8c84c6b5bc180134a1b479e510fca
andreas <caternberg@gmx.de>
2023-02-22 17:20:26 +0100

    upgrade version

--- casc-operationscenter/create_configmap.sh
+++ casc-operationscenter/create_configmap.sh
@@ -2,9 +2,9 @@
 
 NS=cloudbees-core
 
-kubectl create configmap oc-casc-bundle -n $NS --from-file=bundle --dry-run=client -o yaml | kubectl apply -f -
-#kubectl -n $NS  delete cm oc-casc-bundle
-#kubectl -n $NS  create cm  oc-casc-bundle --from-file=bundle
+#kubectl create configmap oc-casc-bundle -n $NS --from-file=bundle --dry-run=client -o yaml | kubectl apply -f -
+kubectl -n $NS  delete cm oc-casc-bundle
+kubectl -n $NS  create cm  oc-casc-bundle --from-file=bundle
 #kubectl -n $NS  create cm  oc-casc-bundle --from-file=bundle --dry-run=client -o yaml
 
 #kubectl -n $NS  delete pod cjoc-0

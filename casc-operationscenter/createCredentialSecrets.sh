386850ad9cfdaf9237f74736b726a5c3237bbd7e
cccaternberg <acaternberg@cloudbees.com>
2024-09-17 12:16:29 +0200

    add secrets

--- casc-operationscenter/createCredentialSecrets.sh
+++ casc-operationscenter/createCredentialSecrets.sh
@@ -6,8 +6,9 @@
 #    --from-literal=githubToken="$(yq '.githubToken' secrets/cbci-secrets.yaml )" \
 #    --from-literal=licenseCert="$(yq '.licenseCert' secrets/cbci-secrets.yaml )" \
 #    --from-literal=licenseKey="$(yq '.licenseKey' secrets/cbci-secrets.yaml )"
-kubectl delete secret oc-casc-secrets
+
 kubectl delete secret oc-secrets
+kubectl delete secret controller-secrets
 #kubectl create secret generic oc-casc-secrets -n "$CBCI_NS" \
 #    --from-literal=ocLogin="$(yq '.ocLogin' secrets/cbci-secrets.yaml )" \
 #    --from-literal=githubUser="$(yq '.githubUser' secrets/cbci-secrets.yaml )" \
@@ -25,3 +26,8 @@ kubectl create secret generic oc-secrets -n "$CBCI_NS" \
     --from-literal=licenseKey="$(yq '.licenseKey' secrets/cbci-secrets.yaml )" \
     --from-literal=awsAccessKey="$(yq '.awsAccessKey' secrets/cbci-secrets.yaml )" \
     --from-literal=awsSecretKey="$(yq '.awsSecretKey' secrets/cbci-secrets.yaml )"
+
+kubectl create secret generic controller-secrets -n "$CBCI_NS" \
+    --from-literal=ghAppId="$(yq '.ghAppId' secrets/cbci-secrets.yaml )" \
+    --from-literal=gitHubAppPrivateKey="$(yq '.gitHubAppPrivateKey' secrets/cbci-secrets.yaml )"
+

5960372f2f780eadc5b60690decfa954c6e17b27
cccaternberg <acaternberg@cloudbees.com>
2025-01-08 14:18:11 +0100

    update

--- casc-operationscenter/createCredentialSecrets.sh
+++ casc-operationscenter/createCredentialSecrets.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+
+export CBCI_NS=cloudbees-core
+#export CBCI_NS=cjoc1
+
 #kubectl create secret generic oc-secrets -n "$CBCI_NS" \
 #    --from-literal=login="$(yq '.login' secrets/cbci-secrets.yaml )" \
 #    --from-literal=githubUser="$(yq '.githubUser' secrets/cbci-secrets.yaml )" \
@@ -7,8 +11,8 @@
 #    --from-literal=licenseCert="$(yq '.licenseCert' secrets/cbci-secrets.yaml )" \
 #    --from-literal=licenseKey="$(yq '.licenseKey' secrets/cbci-secrets.yaml )"
 
-kubectl delete secret oc-secrets
-kubectl delete secret controller-secrets
+kubectl delete secret oc-secrets -n $CBCI_NS
+kubectl delete secret controller-secrets -n $CBCI_NS
 #kubectl create secret generic oc-casc-secrets -n "$CBCI_NS" \
 #    --from-literal=ocLogin="$(yq '.ocLogin' secrets/cbci-secrets.yaml )" \
 #    --from-literal=githubUser="$(yq '.githubUser' secrets/cbci-secrets.yaml )" \
@@ -31,9 +35,13 @@ kubectl create secret generic oc-secrets -n "$CBCI_NS" \
 
 kubectl create secret generic controller-secrets -n "$CBCI_NS" \
     --from-literal=gitHubAppId="$(yq '.gitHubAppId' secrets/cbci-secrets.yaml )" \
-    --from-literal=gitHubAppPrivateKey="$(yq '.gitHubAppPrivateKey' secrets/cbci-secrets.yaml )"
+    --from-literal=gitHubAppPrivateKey="$(yq '.gitHubAppPrivateKey' secrets/cbci-secrets.yaml )" \
+    --from-literal=dockerConfigJson="$(yq '.dockerConfigJson' secrets/cbci-secrets.yaml )"
+
+kubectl delete secret controller-secrets -n cloudbees-controllers
 
 kubectl create secret generic controller-secrets -n cloudbees-controllers \
      --from-literal=gitHubAppId="$(yq '.gitHubAppId' secrets/cbci-secrets.yaml )" \
-     --from-literal=gitHubAppPrivateKey="$(yq '.gitHubAppPrivateKey' secrets/cbci-secrets.yaml )"
+     --from-literal=gitHubAppPrivateKey="$(yq '.gitHubAppPrivateKey' secrets/cbci-secrets.yaml )" \
+      --from-literal=dockerConfigJson="$(yq '.dockerConfigJson' secrets/cbci-secrets.yaml )"
 

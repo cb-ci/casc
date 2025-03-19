048a4f0398febfd4cf715eb04af3e1d716974221
cccaternberg <acaternberg@cloudbees.com>
2025-03-19 15:12:18 +0100

    test

--- patch.sh
+++ patch.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 JENKINS_YAML=${1:-"jenkins.yaml"}
 cp -f $JENKINS_YAML $JENKINS_YAML.bak
-cat $JENKINS_YAML | yq e  'del(.unclassified.location, .unclassified.operationsCenterRootAction, .jenkins.agentProtocols, .license, .jenkins.labelAtoms, .jenkins.remotingSecurity, .jenkins.slaveAgentPort, .jenkins.updateCenter, .credentials, .unclassified.quietStartConfiguration)'  | tee patched-jenkins.yaml
+cat $JENKINS_YAML | yq e  'del(.unclassified.gitHubPluginConfig, .unclassified.location, .unclassified.operationsCenterRootAction, .jenkins.agentProtocols, .license, .jenkins.labelAtoms, .jenkins.remotingSecurity, .jenkins.slaveAgentPort, .jenkins.updateCenter, .credentials, .unclassified.quietStartConfiguration)'  | tee patched-jenkins.yaml
 diff patched-jenkins.yaml $JENKINS_YAML
